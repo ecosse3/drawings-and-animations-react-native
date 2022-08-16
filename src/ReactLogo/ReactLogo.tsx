7247121cc4f23f70fbd6ed683942f43b5b81985a
ecosse3 <luk.kurpiewski@gmail.com>
2022-08-16 21:41:13 +0200

    React Logo: Finish

--- src/ReactLogo/ReactLogo.tsx
+++ src/ReactLogo/ReactLogo.tsx
@@ -9,6 +9,7 @@ import {
   SweepGradient,
   RadialGradient,
   BlurMask,
+  Paint,
 } from "@shopify/react-native-skia";
 import React from "react";
 import { Dimensions } from "react-native";
@@ -25,15 +26,94 @@ const center = vec(width / 2, height / 2);
 const r = 0.75 * PADDING;
 // Rectangle to draw the oval in
 const rct = rect(center.x - rx, center.y - ry, rx * 2, ry * 2);
+const strokeRctOuter = rect(
+  center.x - rx - r / 2,
+  center.y - ry - r / 2,
+  rx * 2 + r,
+  ry * 2 + r
+);
+const strokeRctInner = rect(
+  center.x - rx + r / 2,
+  center.y - ry + r / 2,
+  rx * 2 - r,
+  ry * 2 - r
+);
 // Some colors
 const c1 = "#3884FF";
 const c2 = "#51D3ED";
+const c3 = "#FF8360";
+const c4 = "#F47375";
 const strokeWidth = r;
 
 export const ReactLogo = () => {
   return (
     <Canvas style={{ flex: 1 }}>
       <Fill color="white" />
+      <Group>
+        <BlurMask blur={0.5} style="inner" />
+        <Circle c={center} r={r}>
+          <RadialGradient
+            c={vec(center.x + r / 2, center.y + r / 2)}
+            colors={[c3, c4]}
+            r={2 * r}
+          />
+          <Paint color="black" style="stroke" strokeWidth={3} />
+        </Circle>
+      </Group>
+      <Group>
+        <SweepGradient c={center} colors={[c1, c2, c1]} />
+        <Group transform={[{ scaleX: -1 }]} origin={center}>
+          <Oval rect={rct} style="stroke" strokeWidth={strokeWidth}>
+            <BlurMask blur={1} style="solid" />
+          </Oval>
+          <Oval
+            rect={strokeRctInner}
+            color="black"
+            style="stroke"
+            strokeWidth={3}
+          />
+          <Oval
+            rect={strokeRctOuter}
+            color="black"
+            style="stroke"
+            strokeWidth={3}
+          />
+        </Group>
+        <Group transform={[{ rotate: Math.PI / 3 }]} origin={center}>
+          <Oval rect={rct} style="stroke" strokeWidth={strokeWidth}>
+            <BlurMask blur={1} style="solid" />
+          </Oval>
+          <Oval
+            rect={strokeRctInner}
+            color="black"
+            style="stroke"
+            strokeWidth={3}
+          />
+          <Oval
+            rect={strokeRctOuter}
+            color="black"
+            style="stroke"
+            strokeWidth={3}
+          />
+        </Group>
+        <Group transform={[{ rotate: -Math.PI / 3 }]} origin={center}>
+          <Oval rect={rct} style="stroke" strokeWidth={strokeWidth}>
+            <BlurMask blur={1} style="solid" />
+          </Oval>
+          <Oval
+            rect={strokeRctInner}
+            color="black"
+            style="stroke"
+            strokeWidth={3}
+          />
+          <Oval
+            rect={strokeRctOuter}
+            color="black"
+            style="stroke"
+            strokeWidth={3}
+          />
+        </Group>
+      </Group>
     </Canvas>
   );
 };
